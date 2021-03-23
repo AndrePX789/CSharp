8d754c9d8c98f7b499efd90d78e7670591341618
André Oliveira <andrepx_oliveira@hotmail.com>
2021-03-23 16:35:14 -0300

    first commit

---
+++ Teste/Teste/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            double valorInvestido = 1000;
+            double fatorRendimento = 1.0036;
+
+            for(int contAno = 1; contAno <= 5; contAno++)
+            {
+                for (int contMes = 1; contMes <= 12; contMes++)
+                {
+                    valorInvestido *= fatorRendimento;
+
+                    Console.WriteLine("Você ja tem poupando R$" + valorInvestido);
+                }
+                
+            }
+
+            fatorRendimento += 0.0010;           
+
+        }
+    }
+}
---
+++ Teste/Variaveis/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Variaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Criando Váriaveis");
+
+            int idade = 23;
+            double peso = 62.5;
+
+
+            Console.WriteLine("minha idade é  " + idade + " e tenho " + peso + " quilos.");
+
+            //conversor
+            double comprimento = 20.5;
+            int metro = (int)comprimento;
+
+            //character usar aspas simples
+            char umaLetra = 'a';
+
+
+            umaLetra = (char)65; // return "A" 
+
+            umaLetra = (char)(umaLetra + 1); // return "B"
+
+
+            string texto = "texto";
+
+            //if (condição)  || = or
+            //{                 && = and
+            //    comando;
+            //}
+            //else
+            //{
+            //    comando;
+            //}
+
+            bool boolean = idade >= 20;
+
+            //while (condição)
+            //{
+            //    comando;
+
+            //    condição++;
+            //}
+
+
+
+
+
+
+        }
+    }
+}
